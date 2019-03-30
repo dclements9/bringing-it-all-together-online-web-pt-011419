52a11d53302cf52bf26913279bfcf9eeedadbf36
dylanclements <dclements@protonmail.com>
2019-03-30 12:54:10 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,81 @@
+require 'pry'
+
+class Dog
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(id: nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            );
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+        DROP TABLE dogs
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def update
+        sql = <<-SQL
+        UPDATE dogs
+        SET name = ?, breed = ?
+        WHERE id = ?
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+    
+    def save
+        if self.id
+            self.update
+        else
+            sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+            SQL
+
+            DB[:conn].execute(sql, self.name, self.breed)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
+        self
+    end
+
+    def self.create(name:, breed:)
+        dog = self.new(name: name, breed: breed)
+        dog.save
+    end
+
+    def self.find_by_id(id)
+        sql = <<-SQL
+            SELECT * from dogs
+            WHERE id = ?
+        SQL
+
+        DB[:conn].execute(sql, id).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
+
+    def self.new_from_db(row)  
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
+
+    def self.find_or_create_by
+        
+    end
+end
